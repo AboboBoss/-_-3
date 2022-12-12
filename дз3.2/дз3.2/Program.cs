2460761becdd0eb521373fa1da1e14f5adf34d2d
Artem <eryominav@mail.ru>
2022-12-12 15:31:40 +0300

    1

---
+++ дз3.2/дз3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+    class Program
+    {
+        public static void Main()
+        {
+            double[] x = new double[6];
+            double d, distance = 0.0;
+            Console.WriteLine("x1 y1 z1 x2 y2 z2:");
+            var line = Console.ReadLine().Split();
+            for (int i = 0; i < 3; i++)
+            {
+                d = Convert.ToDouble(line[i + 3])
+            - Convert.ToDouble(line[i]);
+                distance += d * d;
+            }
+            Console.WriteLine("r = " + Math.Sqrt(distance));
+        }
+    }
---
+++ дз3.3/дз3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Введите число N");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($" {i * i * i}, ");
+        }
+
+        Console.ReadKey(true);
+     }
+}
